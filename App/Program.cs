9cecb0c7344c92b3ec5eac3ad1d3f4b655c4b5c6
Fajar Maulana <fjrmaulana@yahoo.com>
2024-08-24 20:37:20 +0700

    Add project files.

---
+++ App/Program.cs
@@ -0,0 +1,8 @@
+﻿var counter = 0;
+var max = args.Length is not 0 ? Convert.ToInt32(args[0]) : -1;
+while (max is -1 || counter < max)
+{
+    Console.WriteLine($"Counter: {++counter}");
+    await Task.Delay(TimeSpan.FromMilliseconds(1_000));
+}
+//ttps://learn.microsoft.com/en-us/dotnet/core/docker/build-container?tabs=linux&pivots=dotnet-8-0
